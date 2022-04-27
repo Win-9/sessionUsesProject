d3a5c1caee5b9b0bf37aa26d3835c322dafdd042
Win_9 <krrr883@gmail.com>
2022-04-27 19:30:52 +0900

    session변경에 따른 controller변경

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -10,10 +10,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -45,7 +47,7 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+    //@GetMapping("/")
     public String homeLoginV2(HttpServletRequest request, Model model) {
 
         //sessoinManger에 저장된 정보조회
@@ -57,6 +59,20 @@ public class HomeController {
         }
 
         model.addAttribute("member", member);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false)Member loginMember,
+                              Model model) {
+
+        if (loginMember == null) {
+            return "home";
+        }
+
+
+        //세션유지되면
+        model.addAttribute("member", loginMember);
         return "loginHome";
     }
 
