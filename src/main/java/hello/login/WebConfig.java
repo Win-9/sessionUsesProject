f23248cf323e6d811a250ecb95a4056f894521bb
Win_9 <krrr883@gmail.com>
2022-06-01 23:51:16 +0900

    PathPattern 추가

--- src/main/java/hello/login/WebConfig.java
+++ src/main/java/hello/login/WebConfig.java
@@ -21,6 +21,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .order(1)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/css/**", "/*.ico", "/error");
+
+        registry.addInterceptor(new LogInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/members/add", "/login", "/css/**", "/*.ico", "/error");
     }
 
     //@Bean
